8098eae0b02d223bddfcfa4db421af21a32aed96
Rimjhim36 <rimjhimkayastha95@gmail.com>
2026-01-08 13:07:24 +0530

    Iitial Commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+const task = document.querySelector("#inp");
+const items = document.querySelector("#list-items");
+function addTask() {
+    // alert("cvbnm,");
+    if (task.value == "") {
+        alert("cannot add empty task");
+        return;
+    }
+    let newitem = document.createElement("li");
+    newitem.innerHTML = `${task.value}<button><i class="fa-solid fa-trash"></i></button>`;
+    items.appendChild(newitem);
+    task.value = "";
+    newitem.querySelector('button').addEventListener('click', () => {
+        newitem.remove();
+        if (items.innerHTML == "")
+        {
+            document.querySelector(`.button2 button`).style.visibility = "hidden";
+        }
+    })
+}
+function clear() {
+    items.innerHTML="";
+    document.querySelector(`button2 button`).style.visibility = "hidden";
+}
